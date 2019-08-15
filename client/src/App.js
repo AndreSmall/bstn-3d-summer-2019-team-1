ed17cda6d1c98fa063f2517c892225f8fa55cf21
Dale Shlass <dale@shlass.com>
2019-08-15 17:45:41 -0400

    Small test change.

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
+      <h1>Test Code</h1>
         <a
           className="App-link"
           href={`http://localhost:8080/ping`}
