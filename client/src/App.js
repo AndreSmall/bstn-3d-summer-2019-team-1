c1b56abd40166c3ced15e0de3b290d1b0734ef11
Dale Shlass <dale@shlass.com>
2019-08-16 14:35:38 -0400

    checkpoint about to pull from master

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 // import Graph from './components/graph';
-import Filter from './components/Filter/Filter'
+// import Filter from './components/Filter/Filter'
+import Main from './components/Main/Main'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
-        <Filter />
+        <Main />
         {/* <iframe src='https://public.tableau.com/views/Book1_test_15659692754240/Sheet4?:showVizHome=no&:embed=true' title='data' width='500' height='500'></iframe> */}
       </div>
     )
