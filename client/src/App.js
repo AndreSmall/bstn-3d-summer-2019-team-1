9b4a7897ab2986e6a79e44e2e741bdd01ee9b05c
Assana <bmisrah@gmail.com>
2019-08-16 14:24:16 -0400

    working on header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import Navigation from "../src/components/Navigation/Navigation"
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <iframe src='https://public.tableau.com/views/Book1_test_15659692754240/Sheet4?:showVizHome=no&:embed=true' title='data' width='500' height='500'></iframe>
+
+        <Navigation/>
       </div>
     )
   }
---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+
+
+class Navigation extends React.Component{
+
+    render(){
+
+    return(
+        <>
+
+            <div className="Top-section">
+                <div className="header">
+                <p className="header__logo">Logo/Company</p> 
+                <input className='navigation__search' type='search' name="search" placeholder='Start your food journey here!'/> 
+
+            </div>  
+
+                </div> 
+
+                <nav class>
+                    <ul>
+                        <li>Name 1</li>
+                        <li>Name 2</li>
+                        <li>Name 3</li>
+                    </ul>
+                </nav> 
+            
+        </> 
+    
+        )
+    }
+}
+
+  
+
+
+export default Navigation;
