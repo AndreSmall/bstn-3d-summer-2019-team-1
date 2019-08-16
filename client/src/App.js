5c59d178653edbae8a279f34cec05210a955c1cf
Dale Shlass <dale@shlass.com>
2019-08-16 13:42:13 -0400

    filter component bare bones completed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+// import Graph from './components/graph';
+import Filter from './components/Filter/Filter'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <iframe src='https://public.tableau.com/views/Book1_test_15659692754240/Sheet4?:showVizHome=no&:embed=true' title='data' width='500' height='500'></iframe>
+        <h1>Hello</h1>
+        <Filter />
+        {/* <iframe src='https://public.tableau.com/views/Book1_test_15659692754240/Sheet4?:showVizHome=no&:embed=true' title='data' width='500' height='500'></iframe> */}
       </div>
     )
   }
